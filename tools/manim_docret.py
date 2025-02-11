2cbfe69c2167cdaa55fd0a166f0be070ba1ce9aa
Abhiram <113409577+ABHCI15@users.noreply.github.com>
2025-02-11 17:49:04 +0000

    uhh slight improvements in comment and removal of async def

--- tools/manim_docret.py
+++ tools/manim_docret.py
@@ -1,14 +1,14 @@
 from langchain_core.tools import tool
 import chromadb.utils.embedding_functions as embedding_functions
 @tool 
-async def manim_search(query:str) -> list:
+def manim_search(query:str) -> list:
   """
   retrieve information from manim documentation to perform task, the quesry should be as if it is a web search within documentation. 
   """
   query_prompt = f"Given a web search query, retrieve relevant passages that answer the query '{query}'"
   huggingface_ef = embedding_functions.SentenceTransformerEmbeddingFunction(
       model_name="NovaSearch/stella_en_1.5B_v5",
-      device = "cuda", # xla for TPU, cuda for GPU, cpu for CPU
+      device = "cuda", # xla for TPU, cuda for GPU, cpu for CPU (still have to get xla to work but cuda takes ~1 second or less, cpu takes ~30 seconds per query)
       trust_remote_code=True
   )
   chroma_client = chromadb.PersistentClient(path="./manim")
