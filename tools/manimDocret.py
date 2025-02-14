8a6174718918257a1181894fa8b827c3777deb63
Noah Bates <noahsbates@gmail.com>
2025-02-13 16:46:21 -0800

    Testing Manim Docret

--- tools/manimDocret.py
+++ tools/manimDocret.py
@@ -1,10 +1,13 @@
 from langchain_core.tools import tool
 import chromadb.utils.embedding_functions as embedding_functions
+import torch
+print(torch.cuda.is_available())
 
 @tool 
 def manimSearch(query:str) -> list:
   """
   Retrieve information from manim documentation to perform task, the query should be as if it is a web search within documentation. 
+  Returns a list of 15 relevant string passages.
   """
   queryPrompt = f"Given a web search query, retrieve relevant passages that answer the query '{query}'"
   huggingface_ef = embedding_functions.SentenceTransformerEmbeddingFunction(
@@ -12,7 +15,9 @@ def manimSearch(query:str) -> list:
       device = "cuda", # xla for TPU, cuda for GPU, cpu for CPU (still have to get xla to work but cuda takes ~1 second or less, cpu takes ~30 seconds per query)
       trust_remote_code=True
   )
-  chromaClient = chromadb.PersistentClient(path="./manim")
+  chromaClient = chromadb.PersistentClient(path="./data/manim")
   collection = chromaClient.get_collection("manim_docs2801", embedding_function=huggingface_ef)
   return collection.query(query_texts=queryPrompt, n_results=15)["documents"]
 
+if __name__ == "__main__":
+  print(manimSearch.invoke("How to create a circle in manim?")[0])
