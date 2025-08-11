2fa34c40fb4cdb10fcbb12994160f9d343087ab8
James Sun <mnpencildoodle@gmail.com>
2025-08-11 09:25:37 -0700

    Add backend shell

--- nextapplication/manimgpt/app/page.tsx
+++ nextapplication/manimgpt/app/page.tsx
@@ -3,6 +3,16 @@ import { Button } from "@/components/ui/button";
 import { Send } from "lucide-react";
 
 export default function Home() {
+  const fetchVideo = async () => {
+    try {
+      // need to change the api url accordingly
+      const response = await fetch(`http://localhost:3000/generatevideo`);
+      const todos = await response.json();
+    } catch (error) {
+      console.error("Error fetching todos:", error);
+    }
+  };
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
